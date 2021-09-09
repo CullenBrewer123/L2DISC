5fd593b8ce28c4269731639ac435155b623b75dd
CullenBrewer123 <78446892+CullenBrewer123@users.noreply.github.com>
2021-09-10 10:51:57 +1200

    Updated Dice Roller Code
    Cleaned up and streamlined.

--- Dice Roller.py
+++ Dice Roller.py
@@ -1,14 +1,13 @@
 import random
 
-dicetype = int(input("What type of dice would you like to roll? "))
-numofdice = int(input("How many dice would you like to roll? "))
+dice_type = int(input("What type of dice would you like to roll? "))
+num_of_dice = int(input("How many dice would you like to roll? "))
 total = 0
-listofdice = []
-for i in range(numofdice):
-    listofdice.append(random.randint(1,dicetype))
-    total += listofdice[-1]
+list_of_dice = []
 
+for i in range(num_of_dice):
+    list_of_dice.append(random.randint(1, dice_type))
+    total += list_of_dice[-1]
 
-
-print(f"The Total Value is ", total)
-print("Individual Dice rolls are:{}".format(listofdice))
+print(f"The Total Value is {total}")
+print(f"Individual Dice rolls are: {list_of_dice}")
