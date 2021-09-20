9e1217b4ce5c0fff6f1434db105c668289ed105d
numat <numat@stu.otc.school.nz>
2021-09-20 14:56:59 +1200

    gooder

--- Dice Roller.py
+++ Dice Roller.py
@@ -1,13 +1,36 @@
+import tkinter as tk
 import random
+import time
 
-dice_type = int(input("What type of dice would you like to roll? "))
-num_of_dice = int(input("How many dice would you like to roll? "))
-total = 0
-list_of_dice = []
+while True:
+    while True:
+        try:
+            print("What type of dice would you like to roll?")
+            dice_type = int(input("D"))
+            break
+        except:
+            print("ERROR")
+            time.sleep(1)
+            continue
 
-for i in range(num_of_dice):
-    list_of_dice.append(random.randint(1, dice_type))
-    total += list_of_dice[-1]
+    while True:
+        try:
+            print("How many dice would you like to roll?")
+            num_of_rolls = int(input())
+            break
+        except:
+            print("ERROR")
+            time.sleep(1)
+            continue
 
-print(f"The Total Value is {total}")
-print(f"Individual Dice rolls are: {list_of_dice}")
+    total = 0
+    list_of_dice = []
+
+    for i in range(num_of_rolls):
+        list_of_dice.append(random.randint(1, dice_type))
+        total += list_of_dice[-1]
+
+    if num_of_rolls != 1:
+        print("The Total Value is {}".format(total))
+    print("Individual Dice rolls are: {}".format(list_of_dice))
+
